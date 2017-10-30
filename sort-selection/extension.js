c24020033eec7df48e20e2df5ce9b4de15fa0b9c
Jim Redfern <jim@tradeshift.com>
2017-10-29 19:14:26 -0700

    1.0.0 release

--- sort-selection/extension.js
+++ sort-selection/extension.js
@@ -1,30 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
+	console.log('"sort-selection" is active!');
+	
+	const disposable = vscode.commands.registerCommand('extension.sortSelection', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return; // No open text editor
+		}		
+		const selection = editor.selection;
+		const document = editor.document;
+		
+		editor.edit((edit) => {
+			const txt = document.getText(new vscode.Range(selection.start, selection.end));
+			edit.replace(selection, sort(txt));
+		});
+	});
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sort-selection" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
+function sort(txt) {
+	const tokens = txt.split(/,\s*/);
+	const sortedTokens = tokens.sort();
+	const sortedText = sortedTokens.join(', ');
+	return sortedText.slice(0, sortedText.length);
 }
-exports.deactivate = deactivate;
+exports.sort = sort;
