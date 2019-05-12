521da7a25ee528024ac24fee5e08e4a41a6f4073
Gustavo Costa <gusbemacbe@gmail.com>
2019-05-12 05:01:49 -0300

    Unified the extensions into one, supporting natural sort

--- sort-selection/extension.js
+++ sort-selection/extension.js
@@ -21,9 +21,27 @@ function activate(context) {
 }
 exports.activate = activate;
 
-function sort(txt) {
+function naturalCompare(a, b) 
+{
+    var ax = [], bx = [];
+
+    a.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { ax.push([$1 || Infinity, $2 || ""]) });
+    b.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { bx.push([$1 || Infinity, $2 || ""]) });
+    
+    while(ax.length && bx.length) {
+        var an = ax.shift();
+        var bn = bx.shift();
+        var nn = (an[0] - bn[0]) || an[1].localeCompare(bn[1]);
+        if(nn) return nn;
+    }
+
+    return ax.length - bx.length;
+}
+
+function sort(txt) 
+{
 	const tokens = txt.split(/,\s*/);
-	const sortedTokens = tokens.sort(Intl.Collator().compare);
+	const sortedTokens = tokens.sort(naturalCompare);
 	const sortedText = sortedTokens.join(', ');
 	return sortedText.slice(0, sortedText.length);
 }
