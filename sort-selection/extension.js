616d358b190e28189a554ea36bfe8914c2f0be70
Gustavo Costa <gusbemacbe@gmail.com>
2021-04-28 22:56:00 -0300

    Refactored and unified

--- sort-selection/extension.js
+++ sort-selection/extension.js
@@ -19,6 +19,7 @@ function activate(context) {
 
 	context.subscriptions.push(disposable);
 }
+
 exports.activate = activate;
 
 // These codes are taken from https://stackoverflow.com/a/15479354, done by Georg Barikin (https://github.com/gebrkn)
@@ -31,20 +32,33 @@ function naturalCompare(a, b)
     b.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { bx.push([$1 || Infinity, $2 || ""]) });
     
     while(ax.length && bx.length) {
-        var an = ax.shift();
-        var bn = bx.shift();
-        var nn = (an[0] - bn[0]) || an[1].localeCompare(bn[1]);
+        const an = ax.shift();
+        const bn = bx.shift();
+        const nn = (an[0] - bn[0]) || an[1].localeCompare(bn[1]);
         if(nn) return nn;
     }
 
     return ax.length - bx.length;
 }
 
-function sort(txt) 
+const isIncludedIn = (text) => (search) => text.includes(search);
+const trim = (text) => text.trim();
+
+const createSortFunction = (validSeparators) => (text) => 
 {
-	const tokens = txt.split(/,\s*/);
-	const sortedTokens = tokens.sort(naturalCompare);
-	const sortedText = sortedTokens.join(', ');
-	return sortedText.slice(0, sortedText.length);
-}
+	const separator = validSeparators.find(isIncludedIn(text));
+	const joiner = `${separator} `;
+
+	if (!separator) throw new Error('Separator not found.');
+
+	return text
+			.split(separator)
+			.map(trim)
+			.filter(Boolean)
+			.sort(naturalCompare)
+			.join(joiner);
+};
+
+const sort = createSortFunction([',', ';', ' -']);
+
 exports.sort = sort;
