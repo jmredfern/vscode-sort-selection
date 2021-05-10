4021cdfc3d1f4f3d6edfbae3395c04ff5323202d
Gustavo Costa <gusbemacbe@gmail.com>
2021-05-09 23:06:06 -0300

    Renamed to `sort-delimited-list`

--- sort-delimited-list/extension.js
+++ sort-delimited-list/extension.js
@@ -1,9 +1,9 @@
 const vscode = require('vscode');
 
 function activate(context) {
-	console.log('"sort-selection" is active!');
+	console.log('"sort-delimited-list" is active!');
 	
-	const disposable = vscode.commands.registerCommand('extension.sortSelection', () => {
+	const disposable = vscode.commands.registerCommand('extension.sortDelimitedList', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return; // No open text editor
